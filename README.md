595dda9f2d84274ce29414e6439dc99b7fe63751
edsonp76 <edsonadriano.pedroso@gmail.com>
2022-04-28 21:35:58 -0300

    inclusao do desafio dio pwa

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Desafio de Projeto sobre PWA com React - covid19
+
 Repositório do desenvolvimento de uma Progressive Web Application (PWA) com React para mapear os dados do COVID19 pelo mundo, com especial atenção para o conjunto de países emergentes denominado BRICS.
+
+# Recriado a partir do Repositório: 
+https://github.com/Tautorn/covid19-dio
+
+Criada conta no Netlify para hospedagem do site a partir do GitHub
---
+++ src/commons/constants/countries.js
@@ -0,0 +1,40 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import RussiaFlag from '../../assets/images/russia.png'
+import IndiaFlag from '../../assets/images/india.png'
+import AfricaFlag from '../../assets/images/africa.png'
+
+
+
+const COUNTRIES = [
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'Russia',
+    value: 'russia',
+    flag: RussiaFlag
+  },  
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'India',
+    value: 'india',
+    flag: IndiaFlag
+  },
+  {
+    label: 'Africa',
+    value: 'africa',
+    flag: AfricaFlag
+  }
+  
+
+  
+]
+
+export default COUNTRIES
